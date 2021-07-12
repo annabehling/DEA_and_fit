9e2a73f4fea398df7353b4fa61f3b5bbd1733e01
Anna Martinson-Behling <anna.behling93@gmail.com>
2021-07-12 15:53:10 +1200

    fixed file paths

--- functions_usage.R
+++ functions_usage.R
@@ -143,10 +143,10 @@ get_nonNA <- function(classes_df){
 ## allopolyploid fungi
 
 # reading in read count data
-hybrid_mod_allo_f <- hybrid_DE("~/Desktop/research/frozen_data/hylite/", "allo_f", include_N = TRUE, 
+hybrid_mod_allo_f <- hybrid_DE("./all_count_matrices", "allo_f", include_N = TRUE, 
                                parent_1 = "E_elymi", parent_2 = "E_amarillans") #hybrid
 
-exp_file_allo_f <- read_exp_file("~/Desktop/research/frozen_data/hylite/", "allo_f") #parent
+exp_file_allo_f <- read_exp_file("./all_count_matrices", "allo_f") #parent
 parent_mod_allo_f <- parental_DE(exp_file_allo_f, parent_1 = "E_elymi", parent_2 = "E_amarillans")
 
 # identify differentially expressed genes
@@ -154,7 +154,7 @@ parent_classes_allo_f <- fit_and_classify(parent_mod_allo_f, parent_1 = "E_elymi
 hybrid_classes_allo_f <- fit_and_classify(hybrid_mod_allo_f, parent_1 = "E_elymi", parent_2 = "E_amarillans", cutoff=1, min_p = 0.05)
 
 classes_df_allo_f <- gene_cats(parent_classes_allo_f, hybrid_classes_allo_f, parent_1 = "E_elymi", parent_2 = "E_amarillans", 
-                               results_dir = "~/Desktop/research/frozen_data/hylite/", species = "allo_f")
+                               results_dir = "./all_count_matrices", species = "allo_f")
 
 # exclude genes with very low coverage
 sub_classes_df_allo_f <- get_nonNA(classes_df_allo_f)
@@ -163,10 +163,10 @@ sub_classes_df_allo_f <- get_nonNA(classes_df_allo_f)
 ## homoploid hybrid fungi
 
 # reading in read count data
-hybrid_mod_hh_f <- hybrid_DE("~/Desktop/research/frozen_data/hylite/", "HH_f", include_N = TRUE, 
+hybrid_mod_hh_f <- hybrid_DE("./all_count_matrices", "HH_f", include_N = TRUE, 
                              parent_1 = "S_paradoxus", parent_2 = "S_cerevisiae") #hybrid
 
-exp_file_hh_f <- read_exp_file("~/Desktop/research/frozen_data/hylite/", "HH_f") #parent
+exp_file_hh_f <- read_exp_file("./all_count_matrices", "HH_f") #parent
 parent_mod_hh_f <- parental_DE(exp_file_hh_f, parent_1 = "S_paradoxus", parent_2 = "S_cerevisiae")
 
 # identify differentially expressed genes
@@ -174,7 +174,7 @@ parent_classes_hh_f <- fit_and_classify(parent_mod_hh_f, parent_1 = "S_paradoxus
 hybrid_classes_hh_f <- fit_and_classify(hybrid_mod_hh_f, parent_1 = "S_paradoxus", parent_2 = "S_cerevisiae", cutoff=1, min_p = 0.05)
 
 classes_df_hh_f <- gene_cats(parent_classes_hh_f, hybrid_classes_hh_f, parent_1 = "S_paradoxus", parent_2 = "S_cerevisiae", 
-                             results_dir = "~/Desktop/research/frozen_data/hylite/", species = "HH_f")
+                             results_dir = "./all_count_matrices", species = "HH_f")
 
 # exclude genes with very low coverage
 sub_classes_df_hh_f <- get_nonNA(classes_df_hh_f)
@@ -183,10 +183,10 @@ sub_classes_df_hh_f <- get_nonNA(classes_df_hh_f)
 ## allopolyploid plants
 
 # reading in read count data
-hybrid_mod_allo_p <- hybrid_DE("~/Desktop/research/frozen_data/hylite/", "allo_p", include_N = TRUE, 
+hybrid_mod_allo_p <- hybrid_DE("./all_count_matrices", "allo_p", include_N = TRUE, 
                                parent_1 = "G_raimondii", parent_2 = "G_arboreum") #hybrid
 
-exp_file_allo_p <- read_exp_file("~/Desktop/research/frozen_data/hylite/", "allo_p") #parent
+exp_file_allo_p <- read_exp_file("./all_count_matrices", "allo_p") #parent
 parent_mod_allo_p <- parental_DE(exp_file_allo_p, parent_1 = "G_raimondii", parent_2 = "G_arboreum")
 
 # identify differentially expressed genes
@@ -194,7 +194,7 @@ parent_classes_allo_p <- fit_and_classify(parent_mod_allo_p, parent_1 = "G_raimo
 hybrid_classes_allo_p <- fit_and_classify(hybrid_mod_allo_p, parent_1 = "G_raimondii", parent_2 = "G_arboreum", cutoff=1, min_p = 0.05)
 
 classes_df_allo_p <- gene_cats(parent_classes_allo_p, hybrid_classes_allo_p, parent_1 = "G_raimondii", parent_2 = "G_arboreum", 
-                               results_dir = "~/Desktop/research/frozen_data/hylite/", species = "allo_p")
+                               results_dir = "./all_count_matrices", species = "allo_p")
 
 # exclude genes with very low coverage
 sub_classes_df_allo_p <- get_nonNA(classes_df_allo_p)
@@ -203,10 +203,10 @@ sub_classes_df_allo_p <- get_nonNA(classes_df_allo_p)
 ## homoploid hybrid plants
 
 # reading in read count data
-hybrid_mod_hh_p <- hybrid_DE("~/Desktop/research/frozen_data/hylite/", "HH_p", include_N = TRUE, 
+hybrid_mod_hh_p <- hybrid_DE("./all_count_matrices", "HH_p", include_N = TRUE, 
                              parent_1 = "G_raimondii", parent_2 = "G_arboreum") #hybrid
 
-exp_file_hh_p <- read_exp_file("~/Desktop/research/frozen_data/hylite/", "HH_p") #parent
+exp_file_hh_p <- read_exp_file("./all_count_matrices", "HH_p") #parent
 parent_mod_hh_p <- parental_DE(exp_file_hh_p, parent_1 = "G_raimondii", parent_2 = "G_arboreum")
 
 # identify differentially expressed genes
@@ -214,7 +214,7 @@ parent_classes_hh_p <- fit_and_classify(parent_mod_hh_p, parent_1 = "G_raimondii
 hybrid_classes_hh_p <- fit_and_classify(hybrid_mod_hh_p, parent_1 = "G_raimondii", parent_2 = "G_arboreum", cutoff=1, min_p = 0.05)
 
 classes_df_hh_p <- gene_cats(parent_classes_hh_p, hybrid_classes_hh_p, parent_1 = "G_raimondii", parent_2 = "G_arboreum", 
-                             results_dir = "~/Desktop/research/frozen_data/hylite/", species = "HH_p")
+                             results_dir = "./all_count_matrices", species = "HH_p")
 
 # exclude genes with very low coverage
 sub_classes_df_hh_p <- get_nonNA(classes_df_hh_p)
@@ -223,10 +223,10 @@ sub_classes_df_hh_p <- get_nonNA(classes_df_hh_p)
 ## allopolyploid animals
 
 # reading in read count data
-hybrid_mod_allo_a <- hybrid_DE("~/Desktop/research/frozen_data/hylite/", "allo_a", include_N = TRUE, 
+hybrid_mod_allo_a <- hybrid_DE("./all_count_matrices", "allo_a", include_N = TRUE, 
                                parent_1 = "S_pyrenaicus", parent_2 = "S_alburnoides") #hybrid
 
-exp_file_allo_a <- read_exp_file("~/Desktop/research/frozen_data/hylite/", "allo_a") #parent
+exp_file_allo_a <- read_exp_file("./all_count_matrices", "allo_a") #parent
 parent_mod_allo_a <- parental_DE(exp_file_allo_a, parent_1 = "S_pyrenaicus", parent_2 = "S_alburnoides")
 
 # identify differentially expressed genes
@@ -234,7 +234,7 @@ parent_classes_allo_a <- fit_and_classify(parent_mod_allo_a, parent_1 = "S_pyren
 hybrid_classes_allo_a <- fit_and_classify(hybrid_mod_allo_a, parent_1 = "S_pyrenaicus", parent_2 = "S_alburnoides", cutoff=1, min_p = 0.05)
 
 classes_df_allo_a <- gene_cats(parent_classes_allo_a, hybrid_classes_allo_a, parent_1 = "S_pyrenaicus", parent_2 = "S_alburnoides", 
-                               results_dir = "~/Desktop/research/frozen_data/hylite/", species = "allo_a")
+                               results_dir = "./all_count_matrices", species = "allo_a")
 
 # exclude genes with very low coverage
 sub_classes_df_allo_a <- get_nonNA(classes_df_allo_a)
@@ -243,10 +243,10 @@ sub_classes_df_allo_a <- get_nonNA(classes_df_allo_a)
 ## homoploid hybrid animals
 
 # reading in read count data
-hybrid_mod_hh_a <- hybrid_DE("~/Desktop/research/frozen_data/hylite/", "HH_a", include_N = TRUE, 
+hybrid_mod_hh_a <- hybrid_DE("./all_count_matrices", "HH_a", include_N = TRUE, 
                              parent_1 = "S_pyrenaicus", parent_2 = "S_alburnoides") #hybrid
 
-exp_file_hh_a <- read_exp_file("~/Desktop/research/frozen_data/hylite/", "HH_a") #parent
+exp_file_hh_a <- read_exp_file("./all_count_matrices", "HH_a") #parent
 parent_mod_hh_a <- parental_DE(exp_file_hh_a, parent_1 = "S_pyrenaicus", parent_2 = "S_alburnoides")
 
 # identify differentially expressed genes
@@ -254,7 +254,7 @@ parent_classes_hh_a <- fit_and_classify(parent_mod_hh_a, parent_1 = "S_pyrenaicu
 hybrid_classes_hh_a <- fit_and_classify(hybrid_mod_hh_a, parent_1 = "S_pyrenaicus", parent_2 = "S_alburnoides", cutoff=1, min_p = 0.05)
 
 classes_df_hh_a <- gene_cats(parent_classes_hh_a, hybrid_classes_hh_a, parent_1 = "S_pyrenaicus", parent_2 = "S_alburnoides", 
-                             results_dir = "~/Desktop/research/frozen_data/hylite/", species = "HH_a")
+                             results_dir = "./all_count_matrices", species = "HH_a")
 
 # exclude genes with very low coverage
 sub_classes_df_hh_a <- get_nonNA(classes_df_hh_a)
